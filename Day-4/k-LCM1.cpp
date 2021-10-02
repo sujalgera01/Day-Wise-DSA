832e26a09008145e84129b26c7e558e1fe2503dd
sujalgera01 <sujal.gera2019@vitstudent.ac.in>
2021-10-02 13:59:15 +0530

    Create k-LCM1.cpp

---
+++ Day-4/k-LCM1.cpp
@@ -0,0 +1,25 @@
+// CODEFORCES
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define long long int ll;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,k;
+		cin>>n>>k;
+		
+		if(n%2 == 1){
+			cout<<n/2<<" "<<n/2<<" "<<1<<endl;
+		}
+		else{
+			if((n/2)%2 == 0)
+				cout<<(n/2)<<" "<<(n/4)<<" "<<(n/4)<<endl; 
+			else
+				cout<<((n/2)-1)<<" "<<((n/2)-1)<<" "<<2<<endl;
+		}
+	}
+}
